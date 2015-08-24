c8cba8d527e5029c3955291d1ec8e62a27517a16
redhuo <jimmytsafg@gmail.com>
2015-08-24 16:25:42 -0600

    listo

--- agregarDocumento.c
+++ agregarDocumento.c
@@ -2,6 +2,8 @@
 #include "agregarDocumento.h"
 #include "stdlib.h"
 #include "string.h"
+#include <errno.h>
+#include <unistd.h>
 
 //Metodo utilizado para agregar un nuevo documento a RIC
 //No tiene entradas
@@ -33,9 +35,19 @@ void agregarUnDocumento(){
   char direccion[80];
   char new[] = "cp ";
   char var[30];
+  char newDos[]=" ";
   int tamano;
-
-
+  char cwd [100];
+  if (getcwd(cwd, sizeof(cwd)) != NULL){
+      fprintf(stdout, "Current working dir: %s\n", cwd);
+  }
+  else{
+      perror("getcwd() error");
+      return;
+  }
+  strcat(cwd,"\\");
+  strcat(cwd,"RIDIR");
+  mkdir(cwd);
 
   //Prints interaccion con usuario
   printf("\t\t______________________________________________________\n");
@@ -46,8 +58,9 @@ void agregarUnDocumento(){
   printf("\t\tY presione cualquier tecla para continuar.\n\t\tDireccion: ---->");
   scanf("%s\n", &direccion);
 
-  strcat(strcat(new, direccion)," /home/juan/Escritorio/RI");
+  strcat(strcat(new, direccion),strcat(newDos, cwd));
   //lee el archivo y lo cierra
+  printf("el new es %s\n",new);
   system(new);
 
   //variable para cargar los datos del archivo
