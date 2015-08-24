533199a14330293efa70cb4b5537786961f114e9
JuanDiegoEspinoza <jespinozasurez@gmail.com>
2015-08-24 13:37:33 -0600

    Agregando nuevos cambios

--- agregarDocumento.c
+++ agregarDocumento.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include "agregarDocumento.h"
 #include "stdlib.h"
+#include "string.h"
 
 //Metodo utilizado para agregar un nuevo documento a RIC
 //No tiene entradas
-//No tiene salidas
+//No tiene salidas 
 
 
 void agregarUnDocumento(){
-  //Se almacena la direccion del archivo
-  char direccion[20];
+  //Se crea un struct
+  struct Metadatos{
+    char title;
+    char creator;
+    char subject;
+    char description;
+    char date;
+    char type;
+    char format;
+    char identifier;
+    char languaje;
+    char contributor;
+    char source;
+    char relation;
+    char coverage;
+    char rights;
+    int size;
+  };
 
-  //variable para cargar los datos del archivo
-  FILE *archivo;
+
+  //Se almacena la direccion del archivo
+  char direccion[80];
+  char new[] = "cp ";
+  char var[30];
+  int tamano;
+
+
 
   //Prints interaccion con usuario
   printf("\t\t______________________________________________________\n");
@@ -20,13 +43,70 @@ void agregarUnDocumento(){
   printf("\t\t______________________________________________________\n\n");
 
   printf("\n\t\tINGRESE LA DIRECCION DEL ARCHIVO QUE DESEA AGREGAR.\n");
-  printf("\t\t Y presione cualquier tecla para continuar.\n\t\tDireccion: ---->");
+  printf("\t\tY presione cualquier tecla para continuar.\n\t\tDireccion: ---->");
   scanf("%s\n", &direccion);
-  printf("\t\tLa direccion del archivo ingresado es: %s\n", direccion);
+  printf("\t\tLa direccion del archivo ingresado es:");
 
+  strcat(strcat(new, direccion)," /home/juan/Escritorio/RI");
   //lee el archivo y lo cierra
-  system("cp /home/jimmy/Desktop/hola.txt /home/jimmy/Desktop/RI");
-  printf("Archivo agregado...");
+  system(new);
+
+  //variable para cargar los datos del archivo
+  FILE *archivo;
+  archivo = fopen(direccion, "r");
+  tamano = sizeof(archivo);
+  fclose(archivo);
+
+  printf("\t\tArchivo agregado...";
+
+  struct Metadatos datosDeArchivo;
+
+
+  printf("\n\t\tIngrese el título del archivo:");
+  scanf("%s\n", &datosDeArchivo.title);
+
+  printf("\n\t\tIngrese el nombre del creador del archivo:");
+  scanf("%s\n", &datosDeArchivo.creator);
+
+  printf("\n\t\tIngrese el asunto del archivo:");
+  scanf("%s\n", &datosDeArchivo.subject);
+
+  printf("\n\t\tIngrese la descripcion archivo:");
+  scanf("%s\n", &datosDeArchivo.description);
+
+  printf("\n\t\tIngrese la fecha del archivo:");
+  scanf("%s\n", &datosDeArchivo.date);
+
+  printf("\n\t\tIngrese el tipo del archivo:");
+  scanf("%s\n", &datosDeArchivo.type);
+
+  printf("\n\t\tIngrese el formato del archivo:");
+  scanf("%s\n", &datosDeArchivo.format);
+
+  printf("\n\t\tIngrese el lenguaje del archivo:");
+  scanf("%s\n", &datosDeArchivo.languaje);
+
+  printf("\n\t\tIngrese algún contribuyente del archivo:");
+  scanf("%s\n", &datosDeArchivo.contributor);
+
+  printf("\n\t\tIngrese la raiz del archivo:");
+  scanf("%s\n", &datosDeArchivo.source);
+
+  printf("\n\t\tIngrese la relacion del archivo:");
+  scanf("%s\n", &datosDeArchivo.relation);
+
+  printf("\n\t\tIngrese el lenguaje del archivo:");
+  scanf("%s\n", &datosDeArchivo.coverage);
+
+  printf("\n\t\tIngrese el lenguaje del archivo:");
+  scanf("%s\n", &datosDeArchivo.coverage);
+
+  printf("\n\t\tIngrese el derechos del archivo:");
+  scanf("%s\n", &datosDeArchivo.rights);
+
+
+  ///home/juan/Desktop/asd.txt
+
 
 
   printf("\n\n");
